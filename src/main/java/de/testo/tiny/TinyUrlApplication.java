9d535f8b6ab20ed2178599f8d871448020f96ba4
Mihai-Aurelian Dumitrescu <mihai-aurelian.dumitrescu@zalando.de>
2017-08-21 20:15:36 +0200

    Added swagger file

---
+++ src/main/java/de/testo/tiny/TinyUrlApplication.java
@@ -0,0 +1,11 @@
+package de.testo.tiny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyUrlApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TinyUrlApplication.class, args);
+    }
+}
