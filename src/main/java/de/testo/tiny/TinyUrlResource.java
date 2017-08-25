6029c99bc5a65ec19ad9e8ca4f8c9fbd533237de
Mihai-Aurelian Dumitrescu <mihai-aurelian.dumitrescu@zalando.de>
2017-08-25 17:15:27 +0200

    Add test setup

---
+++ src/main/java/de/testo/tiny/TinyUrlResource.java
@@ -0,0 +1,7 @@
+package de.testo.tiny;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TinyUrlResource {
+}
---
+++ src/test/java/de/testo/tiny/TinyUrlResourceTest.java
@@ -0,0 +1,17 @@
+package de.testo.tiny;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TinyUrlResourceTest {
+
+    @Test
+    public void initialization() {
+        System.out.println("All ok");
+    }
+
+}
